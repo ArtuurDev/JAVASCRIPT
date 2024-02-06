ba8aa2fe415c5a46879b8b743c5f67030caf8fdf
artuur <arturcastrodossantos.com@gmail.com>
2024-02-06 16:35:15 -0300

    adicionado promp-sync

---
+++ aula02.6/aulaInicial.js
@@ -0,0 +1,24 @@
+/*
+
+*/
+
+
+
+
+let primeiraVariavel = 'Amontada Valley'
+
+console.log(primeiraVariavel)
+
+let a = 5, b = 10, c = 20
+console.log(a+b+c)
+
+a = '5', b = 10, c = 20
+
+console.log(a+b+c)
+
+
+const prompt = require('prompt-sync')()
+
+let nome = prompt('informe seu nome:')
+
+console.log(nome)

d88c90d7591947feabeb3d39075a6f2bcf5829a7
artuur <arturcastrodossantos.com@gmail.com>
2024-02-08 23:07:05 -0300

    atualizado diretorio

---
+++ atividadesTreino.js/um.js
@@ -0,0 +1,20 @@
+// mostre o maior e o menor numero 
+let n = 3, n2 = 9, n3 = 8, n4 = 10, n5 = 2
+
+let numeros = [n3,n2,n, n5, n4]
+
+
+
+let maior = 0
+let menor = 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+for (let num in numeros) {
+    if (numeros[num] >= maior) {
+        maior = numeros[num]
+}   else if (numeros[num] < menor) {
+    menor = numeros[num]
+
+}
+}
+console.log(maior)
+console.log(menor)
